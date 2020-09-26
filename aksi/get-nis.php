b6c6ffcdb43f3d537f0ecd6a103363ffbf057360
fadilHidayatul <bangtans46@gmail.com>
2020-09-26 10:52:31 +0700

    order nis

--- aksi/get-nis.php
+++ aksi/get-nis.php
@@ -6,7 +6,7 @@
 
     $chk_nis = isset($_POST['chk-nis']) ? $_POST['chk-nis'] : die();
 
-    $query = "SELECT * FROM tb_nis WHERE kelas = :kelas";
+    $query = "SELECT * FROM tb_nis WHERE kelas = :kelas ORDER BY nis";
     $statement = $conn->prepare($query);
     $statement->bindParam(":kelas", $chk_nis);
     $statement->execute();
